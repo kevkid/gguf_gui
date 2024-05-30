448b609b7d5a927cff5a3c23c4aedce8296f877f
Sam McLeod <sammcj@users.noreply.github.com>
2024-05-30 13:32:46 +1000

    improve run.sh

--- run.sh
+++ run.sh
@@ -1,27 +1,34 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -euo pipefail
 
 # Repository name
 REPO_NAME="llama.cpp"
 
 # Function to check if a directory is a git repository
 is_git_repo() {
-    git -C "$1" rev-parse --is-inside-work-tree > /dev/null 2>&1
+  git -C "$1" rev-parse --is-inside-work-tree >/dev/null 2>&1
 }
 
 # Check if the repository directory exists and is a git repository
 if [ -d "$REPO_NAME" ] && is_git_repo "$REPO_NAME"; then
-    echo "The $REPO_NAME repository already exists in the current directory."
+  echo "The $REPO_NAME repository already exists in the current directory."
 else
-    echo "No $REPO_NAME repository found in the current directory. Cloning repository..."
-    git clone https://github.com/ggerganov/llama.cpp.git
+  echo "No $REPO_NAME repository found in the current directory. Cloning repository..."
+  git clone https://github.com/ggerganov/llama.cpp.git --depth=1
 fi
 
-
+pip install --upgrade huggingface_hub pip streamlit watchdog
 pip install -r "llama.cpp/requirements/requirements-convert-hf-to-gguf.txt"
 cd llama.cpp
+
+# if CUDA is available, set the llama.cpp build options
+if command -v nvcc &>/dev/null; then
+  export LLAMA_CUDA=1
+fi
+
 # you can add in your build options here
-make -j 2
+make -j "$(nproc)"
 cd ..
-pip install streamlit
-pip install --upgrade huggingface_hub
+
 streamlit run main.py
